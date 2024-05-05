81121d815aab30099ec8128ad59151e4f1f603b9
sinasezza <s.e.sezza121@gmail.com>
2024-05-05 13:31:42 +0330

    auto retrie of running tasks on failure implemented.

---
+++ backend/core/celery_tasks/ex3_auto_retry.py
@@ -0,0 +1,27 @@
+import logging
+
+from celery import Task
+
+from core.celery_config import app
+
+"""
+from core.celery_tasks.ex3_auto_retry import my_task
+my_task.delay()
+"""
+
+logging.basicConfig(filename='app.log', level=logging.ERROR, format='%(actime)s %(levelname)s %(message)s')
+
+class CustomTask(Task):
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        if isinstance(exc, ConnectionError):
+            logging.error('Connection error occurred - Admin Notified')
+        else:
+            print('{0!r} failed: {1!r}'.format(task_id, exc))
+            # Perform additional error handling actions if needed
+
+app.Task = CustomTask
+
+@app.task(queue='tasks', autoretry_for=(ConnectionError,), default_retry_delay=5, retry_kwargs={'max_retries': 5})
+def my_task():
+    raise ConnectionError("Connection Error Occurred......")
+    return
